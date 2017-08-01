a83a4ebe83f86c4d2d5ecd37ee93e53ead00d5ac
Magiczny Czarodziej <nexit07@gmail.com>
2017-08-02 00:55:06 +0200

    Add finding locker

--- js/sections-scripts/find-locker.js
+++ js/sections-scripts/find-locker.js
@@ -3,21 +3,56 @@ const jsonq = require('json-query');
 $('#find-locker-inputs').keyup(() => {
   let number, surname, name, classCode;
   number = $('#find-locker-number').val();
-  surname = $('#find-locker-surname').val();
-  name = $('#find-locker-name').val();
-  classCode = $('#find-locker-class').val();
+  surname = $('#find-locker-surname').val().toLowerCase();
+  name = $('#find-locker-name').val().toLowerCase();
+  classCode = $('#find-locker-class').val().toLowerCase();
 
-  let foundLockers = jsonq(`[*nr=${number}]`, {data: lockers}).value;
+  let foundLockers = lockers;
 
-  // document.getElementById('found-lockers-list').innerHTML = "";
-  // let ownerString = [];
-  // for(let item of foundLockers){
-  //   console.log(item);
-  //   for(let owner of item.owners){
-  //     ownerString[] = `${owner.surname} ${owner.name} ${owner.class}`;
-  //   }
-  //   let element = `<div class="found-locker"><div class="found-locker-number">${item.number}</div><div class="found-locker-owner">dgfhgfh</div><div class="found-locker-owner">Hanc Tomasz 4IA</div><button type="button" name="edit-locker" class="general-btn">Edytuj</button></div>`;
-  //   // document.getElementById('found-lockers-list').appendChild(element);
-  // }
-  // console.log(foundLockers);
+  if(number) foundLockers = checkNumber();
+  if(surname) foundLockers = checkSurname();
+  if(name) foundLockers = checkName();
+  if(classCode) foundLockers = checkClass();
+
+  $('#found-lockers-list').html("");
+  for(let locker of foundLockers){
+    let lockerDOM = $(`<div class="found-locker"><div class="found-locker-number">${locker.nr}</div></div>`);
+    for(let owner of locker.owners){
+      let ownerDOM = $(`<div class-"found-locker-owner">${owner.surname} ${owner.name} ${owner.class}</div>`);
+      lockerDOM.append(ownerDOM);
+    }
+    lockerDOM.append('<button type="button" name="edit-locker" class="general-btn">Edytuj</button>');
+    $('#found-lockers-list').append(lockerDOM);
+  }
+
+  function checkNumber() {
+    return jsonq(`[*nr~/^${number}/]`, {data: lockers, allowRegexp: true}).value;
+  }
+
+  function checkSurname() {
+    return foundLockers.filter(locker => {
+      for(let owner of locker.owners){
+        if(owner.surname.startsWith(surname)) return true;
+      }
+      return false;
+    });
+  }
+
+  function checkName() {
+    return foundLockers.filter(locker => {
+      for(let owner of locker.owners){
+        if(owner.name.startsWith(name)) return true;
+      }
+      return false;
+    });
+  }
+
+  function checkClass() {
+    return foundLockers.filter(locker => {
+      for(let owner of locker.owners){
+        if(owner.class.startsWith(classCode)) return true;
+      }
+      return false;
+    });
+  }
 });
